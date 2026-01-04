280ba72ff7031ffe22896e6b752ce8f0c5ea4ff6
VurtsGD <vgd2001@yandex.ru>
2026-01-04 16:08:12 +0600

    Обновлён README: добавлено описание аутентификации

---
+++ src/test/java/com/example/bankcards/BankRestTest.java
@@ -0,0 +1,12 @@
+package com.example.bankcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BankRestTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
